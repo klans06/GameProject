7c1519d9de79310991a7a08c0e2819f80bff212f
Kieran <kierankieranlane@outlook.com>
2026-09-26 02:49:57 +0100

    Added PlayerState support to AssetManager.cs

--- Source/AssetManager.cs
+++ Source/AssetManager.cs
@@ -4,17 +4,35 @@ namespace GameProject.Source;
 
 public class AssetManager
 {
-    private readonly Dictionary<ETileType, Texture2D> _tilesTextures = new();
+    private readonly Dictionary<TileType, Texture2D> _tilesTextures = new();
+    private readonly Dictionary<PlayerState, Texture2D> _playerSkins = new();
 
     public void LoadContent()
     {
-        _tilesTextures[ETileType.Grass] = Raylib.LoadTexture("./Assets/tile_0000.png");
-        _tilesTextures[ETileType.StoneWallBorder] = Raylib.LoadTexture("./Assets/tile_0109.png");
+        // Map Tiles
+        _tilesTextures[TileType.Grass] = Raylib.LoadTexture("./Assets/tile_0000.png");
+        _tilesTextures[TileType.StoneWallBorder] = Raylib.LoadTexture("./Assets/tile_0109.png");
+        
+        // Player State
+        _playerSkins[PlayerState.Default] = Raylib.LoadTexture("./Assets/character_maleAdventurer_side.png");
+        
+        // Building Tiles
+        
     }
 
-    public Texture2D GetTexture(ETileType type)
+    public Texture2D GetMapTexture(TileType type)
     {
         if (_tilesTextures.TryGetValue(type, out Texture2D texture))
+        {
+            return texture;
+        }
+
+        throw new KeyNotFoundException($"No texture for {type}");
+    }
+
+    public Texture2D GetPlayerTexture(PlayerState type)
+    {
+        if (_playerSkins.TryGetValue(type, out Texture2D texture))
         {
             return texture;
         }
