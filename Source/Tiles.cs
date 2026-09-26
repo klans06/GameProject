6c6dfad7ee879b181f5ce2a969c854b3142f5236
Kieran <kierankieranlane@outlook.com>
2026-09-26 02:53:53 +0100

    Added Player State and BuildingTile enum

--- Source/Tiles.cs
+++ Source/Tiles.cs
@@ -1,10 +1,32 @@
 namespace GameProject.Source;
 
-public enum ETileType
+public enum TileType
 {
     Grass, // 0
     GrassWFlower,
     GrassWFlowerPatch,
     Dirt,
     StoneWallBorder // 4
+}
+
+public enum PlayerState
+{
+    Default, // 0
+    WalkingDown,
+    WalkingUp,
+    WalkingLeft,
+    WalkingRight
+    // Walk0,
+    // Walk1,
+    // Walk2,
+    // Walk3,
+    // Walk4,
+    // Walk5,
+    // Walk6,
+    // Walk7 // 6
+}
+
+public enum BuildingTile
+{
+    
 }
