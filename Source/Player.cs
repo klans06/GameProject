95443349d7854e6845242c1a6ea789787d199519
Kieran <kierankieranlane@outlook.com>
2026-09-26 02:53:09 +0100

    Implemented player class with movement

---
+++ Source/Player.cs
@@ -0,0 +1,83 @@
+using System.Numerics;
+using Raylib_cs;
+
+namespace GameProject.Source;
+
+public class Player(int posX, int posY, int speed, float playerScale, PlayerState state, AssetManager assetManager)
+{
+    public int PositionX { get; private set; } = posX;
+    public int PositionY { get; private set; } = posY;
+    public int Speed { get; private set; } = speed;
+    private float PlayerScale { get; set; } = playerScale;
+    public Vector2 PlayerPosition = new Vector2(posX, posY);
+    public PlayerState State { get; private set; } = state;
+    private AssetManager AssetManager { get; } = assetManager;
+
+    public void InitializePlayer()
+    {
+        Texture2D texture = AssetManager.GetPlayerTexture(State);
+        Vector2 position = new Vector2(PositionX, PositionY);
+        
+        // Raylib.DrawTexture(texture, PositionX, PositionY, Color.RayWhite);
+        Raylib.DrawTextureEx(texture, position, 360f, PlayerScale, Color.RayWhite);
+    }
+
+    public void UpdatePosition()
+    {
+        bool isMoving = false;
+        if (Raylib.IsKeyDown(KeyboardKey.W))
+        {
+            PositionY -= Speed;
+            State = PlayerState.Default;
+            PaintPlayer();
+            isMoving = true;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.S))
+        {
+            PositionY += Speed;
+            State = PlayerState.Default;
+            PaintPlayer();
+            isMoving = true;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.A))
+        {
+            PositionX -= Speed;
+            State = PlayerState.Default;
+            PaintPlayer();
+            isMoving = true;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.D))
+        {
+            PositionX += Speed;
+            State = PlayerState.Default;
+            PaintPlayer();
+            isMoving = true;
+        }
+
+        if (!isMoving)
+        {
+            State = PlayerState.Default;
+            PaintPlayer();
+            // Skin = Skin switch
+            // {
+            //     PlayerSkin.WalkingLeft => PlayerSkin.IdleRight,
+            //     PlayerSkin.WalkingRight => PlayerSkin.IdleLeft,
+            //     PlayerSkin.WalkingForward => PlayerSkin.IdleForward,
+            //     PlayerSkin.WalkingBackward => PlayerSkin.IdleBackward,
+            //     _ => PlayerSkin.Default
+            // };
+        }
+    }
+
+    private void PaintPlayer()
+    {
+        Texture2D texture = AssetManager.GetPlayerTexture(State);
+        Vector2 position = new Vector2(PositionX, PositionY);
+        
+        // Raylib.DrawTexture(texture, PositionX, PositionY, Color.RayWhite);
+        Raylib.DrawTextureEx(texture, position, 360f, PlayerScale, Color.RayWhite);
+    }
+}
